706e0cf4ad433147ade85515b04c01b8ad6c9426
Pablo <pabloisaacruizdelgado1@gmail.com>
2024-09-08 14:33:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const registerLink = document.getElementById('register-link');
+const loginLink = document.getElementById('login-link');
+
+registerLink.addEventListener('click', () => {
+  loginForm.style.display = 'none';
+  registerForm.style.display = 'block';
+});
+
+loginLink.addEventListener('click', () => {
+  registerForm.style.display = 'none';
+  loginForm.style.display = 'block';
+});
+
